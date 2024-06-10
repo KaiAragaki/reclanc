fb9fd57ca8702d836df1794442e6f9aac2ef42f4
Kai Aragaki <adamaragaki@gmail.com>
2024-06-10 09:36:28 -0400

    first pass at re-styling

--- R/clanc.R
+++ R/clanc.R
@@ -118,10 +118,10 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
   cv.err.prpn.ttl = cv.err.cnt.ttl / n
 
   printClanc("\n")
-  return(list("classErrors" = cv.err.prpn.cls, "overallErrors" = cv.err.prpn.ttl, "prior" = pi.k))
+  list("classErrors" = cv.err.prpn.cls, "overallErrors" = cv.err.prpn.ttl, "prior" = pi.k)
 }
 
-"buildClanc" <- function(data, id, cNames, train, active) {
+buildClanc <- function(data, id, cNames, train, active) {
   m = nrow(data)
   n = ncol(data)
   p = length(unique(id))
@@ -163,7 +163,7 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
   return(out)
 }
 
-"predictClanc" <- function(data, geneNames, fit) {
+predictClanc <- function(data, geneNames, fit) {
   n = ncol(data)
   cntrds = fit$cntrds
 
@@ -183,7 +183,7 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
   return(cls)
 }
 
-"selectClanc" <- function(d.k, d.k.ord, active) {
+selectClanc <- function(d.k, d.k.ord, active) {
   m = nrow(d.k)
   p = ncol(d.k)
   d.k.rnks = d.k.ord$d.k.rnks
@@ -233,7 +233,7 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
   return(selected)
 }
 
-"testClanc" <- function(data, id, geneNames, fit) {
+testClanc <- function(data, id, geneNames, fit) {
   m = nrow(data)
   n = ncol(data)
   p = ncol(fit$cntrds)
@@ -256,11 +256,10 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
   return(drop(apply(error, 2, sum)))
 }
 
-"trainClanc" <- function(data, id, geneNames, prior = "equal") {
-  ## data: expression data.  (m x n) matrix of class numeric.
-  ## id: class id's.  n vector of class numeric.
-  ## geneNames: gene names.  m vector of class character.
-
+#' @param data expression data. (m x n) matrix of class numeric.
+#' @param id class id's. n vector of class numeric.
+#' @param geneNames gene names. m vector of class character.
+trainClanc <- function(data, id, geneNames, prior = "equal") {
   m = nrow(data)
   n = ncol(data)
   p = length(unique(id))
@@ -280,14 +279,20 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
   d.k = scale((cntrd.k - cntrd.o) / p.sd, center = F, scale = m.k)
   d.k.ord = orderStatsClanc(d.k = d.k)
 
-  out = list("pooledSD" = p.sd, "classCntrds" = cntrd.k, "overallCntrd" = cntrd.o, "tStats" = d.k,
-    "tStatsOrdered" = d.k.ord$d.k.o, "tStatsOrderIndices" = d.k.ord$d.k.rnks, "tStatsNoTies" = d.k.ord$no.ties,
-    "geneNames" = geneNames, "prior" = prior)
-
-  return(out)
+  list(
+    pooledSD = p.sd,
+    classCntrds = cntrd.k,
+    overallCntrd = cntrd.o,
+    tStats = d.k,
+    tStatsOrdered = d.k.ord$d.k.o,
+    tStatsOrderIndices = d.k.ord$d.k.rnks,
+    tStatsNoTies = d.k.ord$no.ties,
+    geneNames = geneNames,
+    prior = prior
+  )
 }
 
-"orderStatsClanc" <- function(d.k) {
+orderStatsClanc <- function(d.k) {
   m = nrow(d.k)
   p = ncol(d.k)
 
@@ -295,10 +300,10 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
   d.k.o = matrix(d.k[as.numeric(d.k.rnks) + m * rep(0:(p - 1), each = m)], nrow = m)
   no.ties = apply(d.k.o, 2, function(x) { !duplicated(x) })
 
-  return(list("d.k.rnks" = d.k.rnks, "d.k.o" = d.k.o, "no.ties" = no.ties))
+  list(d.k.rnks = d.k.rnks, d.k.o = d.k.o, no.ties = no.ties)
 }
 
-"pooledSDClanc" <- function(X, ID) {
+pooledSDClanc <- function(X, ID) {
   m = nrow(X)
   n = ncol(X)
   p = ncol(ID)
@@ -306,12 +311,11 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
 
   mn = t(t(X %*% ID) / nn)
   dif2 = (X - mn %*% t(ID)) ^ 2
-  psd = sqrt(drop(dif2 %*% rep(1 / (n - p), n)))
 
-  return(psd)
+  sqrt(drop(dif2 %*% rep(1 / (n - p), n)))
 }
 
-"balancedFolds" <- function(y, nfolds = 5) {
+balancedFolds <- function(y, nfolds = 5) {
   permuteRows = function(x) {
     dd = dim(x)
     n = dd[1]
@@ -336,10 +340,10 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
     res[[j]] = smallmat[jj, j]
   }
 
-  return(res)
+  res
 }
 
-"distClanc" <- function(data, cntrds, sd, prior) {
+distClanc <- function(data, cntrds, sd, prior) {
   vv = sd ^ 2
   pi.k = prior
 
@@ -349,7 +353,9 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
     dd = drop(cntrds ^ 2 / vv) - 2 * drop(data * cntrds / vv) - 2 * log(pi.k)
 
   return(dd)
-}#' Get ClaNA Group info
+}
+
+#' Get ClaNC Group info
 #'
 #' Extract which group each point belongs to
 #' @param Build_out
@@ -358,110 +364,95 @@ cvClanc <- function(data, id, prior = "equal", active = 1:10, gui = F, prntFrm =
 #' @examples
 #' get_ClaNC_group()
 get_ClaNC_group <- function(Build_out){
-	idx <- apply(Build_out$cntrds, 1, function(x) which.max(abs(x-Mode(x))))
-	summary <- sapply(1:max(idx), function(x) names(idx[idx==x]))
-	colnames(summary) <- paste0("Group", 1:ncol(summary))
-	return(summary)
+  idx <- apply(Build_out$cntrds, 1, function(x) which.max(abs(x-Mode(x))))
+  summary <- sapply(1:max(idx), function(x) names(idx[idx==x]))
+  colnames(summary) <- paste0("Group", 1:ncol(summary))
+  summary
 }
 
-
-#' ClaNC run
-#'
-#' This function allows you to express your love of cats.
-#' @param love Do you love cats? Defaults to TRUE.
-#' @keywords cats
-#' @export
+#' @param data expression data, row(genes), column(samples)
+#' @param groups assigned groups for each column(samples) in data
+#' @param active.features how many features we want to randomly select to test
+#'   cv.errors
+#' @param est.num how many times we want to run the test for cv.errors
+#' @param select.features how many features(genes) we want to select from each
+#'   group for the final results
+#' @param skip.est do you want to skip estimate cv errors and go straight to
+#'   extract features from each group
 #' @examples
-#' cat_function()
-run_ClaNC <- function(data, groups, ColSideColors, active.features=20, est.num=20, select.features=10, file.prefix="ClaNC", skip.est=F){
-# data - expression data, row(genes), column(samples)
-# groups - assigned groups for each column(samples) in data
-# active.features - how many features we want to randomly select to test cv.errors
-# est.num - how many times we want to run the test for cv.errors
-# select.features - how many features(genes) we want to select from each group for the final results
-# skip.est - do you want to skip estimate cv errors and go straight to extract features from each group
+#'
+#' # Load the example data sets in the "data" directory. These are simulated
+#' # microarray data sets, consisting of 10 arrays each from 4 classes; the
+#' # first 10 arrays are from class 1, the second 10 from class 2, etc. And each
+#' # array has 1500 genes on it. The "train" data set is intended for training
+#' # purposes; these are the data you would use in training and building your
+#' # classifier. The "test" data are intended for testing a classifier, using
+#' # data that was not used in the classifier building process. You may or may
+#' # not have test data. If you intend to use all of your samples to build the
+#' # classifier and rely on cross-validation to estimate the resulting error
+#' # rates, you need not have *any* test data.
+#'
+#' Y_train = read.delim("data/trainExample.txt", header = T)
+#' Y_test = read.delim("data/testExample.txt", header = T)
+#' rownames(Y_train) = Y_train[, 1]
+#' rownames(Y_test) = Y_test[, 1]
+#' Y_train = as.matrix(Y_train[, -1])
+#' Y_test = as.matrix(Y_test[, -1])
+#'
+#' # Create vector for class membership. For these example data, we just need 10
+#' # ones followed by 10 twos, then 10 threes, then 10 fours.
+#'
+#' id = rep(1:4, each = 10)
+#' #> Levels: Basal Her2 LumA LumB Normal
+#'
+#' # The gene names are just the row names from the data matrices, the same for
+#' # both the example train and test data sets.
+#' gene_names = rownames(Y_train)
+#'
+#' # We'll just set the class names to be the numbers 1, 2, 3, 4.
+#' class_names = 1:4
+#'
+#' # Specify how many active features(genes) to select from each group Now ready
+#' # to use cross-validation to estimate the error rates for classifiers of
+#' # different sizes (different numbers of genes used in building the
+#' # classifier). THEN View the estimated error rates associated with different
+#' # feature-set sizes. By default, cvClanc() will assess the classifiers built
+#' # using 1, 2, 3, ..., 10 features. In this example, an estimated
+#' # 100% accuracy is attained with as few as 5 features per class.
+#'
+#' # Assuming we're happy with using 5 features per class as the basis for our
+#' # model, we can now train and build our final classifier.
+#'
+run_ClaNC <- function(data, groups, ColSideColors, active.features=20, est.num=20, select.features=10, file.prefix="ClaNC", skip.est=FALSE){
 
-## Load the example data sets in the "data" directory.  These are simulated
-## microarray data sets, consisting of 10 arrays each from 4 classes; the
-## first 10 arrays are from class 1, the second 10 from class 2, etc.  And
-## each array has 1500 genes on it.  The "train" data set is intended for
-## training purposes; these are the data you would use in training and
-## building your classifier.  The "test" data are intended for testing a
-## classifier, using data that was not used in the classifier building
-## process.  You may or may not have test data.  If you intend to use all of
-## your samples to build the classifier and rely on cross-validation to
-## estimate the resulting error rates, you need not have *any* test data.
+  Y_train <- as.matrix(data)
+  idd <- as.numeric(factor(groups))
+  gene_names <- rownames(Y_train)
+  class_names <- 1:length(unique(idd))
 
-# Y_train = read.delim("data/trainExample.txt", header = T)
-# Y_test = read.delim("data/testExample.txt", header = T)
-# rownames(Y_train) = Y_train[, 1]
-# rownames(Y_test) = Y_test[, 1]
-# Y_train = as.matrix(Y_train[, -1])
-# Y_test = as.matrix(Y_test[, -1])
-   Y_train <- as.matrix(data)
+  if (!skip.est){
+    cv_total <- NULL
+    cv_out <- NULL
+    for (i in 1:est.num){
+      cv_out = cvClanc(Y_train, idd, active=1:active.features, prior="class")
+      cv_total = rbind(cv_total, cbind(1:active.features, cv_out$overallErrors))
+    }
+    colnames(cv_total) <- c("NumFeatures", "CV_Error")
+    file.prefix <- paste0(file.prefix, ".estnum", est.num, ".act", active.features)
+    cv_total <- as.data.frame(cv_total)
+    p <- ggplot(data=cv_total, aes(x=factor(NumFeatures), y=CV_Error))
+    p <- p + geom_boxplot(aes(fill = factor(NumFeatures)))
+    p <- p + xlab("Number of Features")
+    pdf(paste0(file.prefix, ".pdf"), height=10, width=16)
+    print(p)
+    dev.off()
+  }
 
-## Create vector for class membership.  For these example data, we just
-## need 10 ones followed by 10 twos, then 10 threes, then 10 fours.
-# id = rep(1:4, each = 10)
-#Levels: Basal Her2 LumA LumB Normal
-      idd <- as.numeric(factor(groups))
-## The gene names are just the row names from the data matrices, the same
-## for both the example train and test data sets.
-# gene_names = rownames(Y_train)
-      gene_names <- rownames(Y_train)
-## We'll just set the class names to be the numbers 1, 2, 3, 4.
-# class_names = 1:4
-      class_names <- 1:length(unique(idd))
-## Specify how many active features(genes) to select from each group
-      active.features <- active.features
-
-## Now ready to use cross-validation to estimate the error rates for
-## classifiers of different sizes (different numbers of genes used in
-## building the classifier).
-## THEN View the estimated error rates associated with different feature-set
-## sizes.  By default, cvClanc() will assess the classifiers built using 1,
-## 2, 3, ..., 10 features.  In this example, an estimated 100% accuracy is
-## attained with as few as 5 features per class.
-      if (skip.est!=T){
-         cv_total <- NULL
-         cv_out <- NULL
-         for (i in 1:est.num){
-               cv_out = cvClanc(Y_train, idd, active=1:active.features, prior="class")
-				# plot(1:active.features, cv_out$overallErrors, type = "l", lwd = 2, col = "blue", xlab = "Number of features", ylab = "CV error")
-                cv_total = rbind(cv_total, cbind(1:active.features, cv_out$overallErrors))
-				# qplot(1:active.features, cv_out$overallErrors, type = "l", lwd = 2, col = "blue", xlab = "Number of features", ylab = "CV error")
-            }
-         colnames(cv_total) <- c("NumFeatures", "CV_Error")
-            file.prefix <- paste0(file.prefix, ".estnum", est.num, ".act", active.features)
-            cv_total <- as.data.frame(cv_total)
-            p <- ggplot(data=cv_total, aes(x=factor(NumFeatures), y=CV_Error))
-            p <- p + geom_boxplot(aes(fill = factor(NumFeatures)))
-            p <- p + xlab("Number of Features")
-            pdf(paste0(file.prefix, ".pdf"), height=10, width=16)
-            print(p)
-            dev.off()
-      }
-
-## Assuming we're happy with using 5 features per class as the basis for
-## our model, we can now train and build our final classifier.
-   file.prefix <- paste0(file.prefix, ".sel", select.features)
-      train_out = trainClanc(Y_train, idd, gene_names)
-# print(train_out$classCntrds)
-      build_out = buildClanc(Y_train, idd, class_names, train_out, active = select.features)
-      write.table(build_out$geneNames, paste0(file.prefix, ".genelist.txt"), row.names = F, quote=F)
-
-      clanc.group <- get_ClaNC_group(build_out)
-      write.csv(clanc.group, paste0(file.prefix, ".group_features.csv"), quote=F, row.names=F)
-
-### Expression profile for genes that overlapped with PAM50
-      ClaNC.genes <- build_out$geneNames
-# plot_CalNC_pam50(build_out, data, groups=groups, file.prefix=paste0(file.prefix, ".overlap.PAM50.geneExp"))
-### Heatmap expression profile for extracted genes(features)
-      sub.data <- extract_data_by_genelist(data, ClaNC.genes)
-      myHeatmap(data=sub.data, color = color, ColSideColors = ColSideColors, file.prefix=paste0(file.prefix, ".extracted.heatmap"), save.image=T, scale="row")
-
-      return(build_out)
-## And if we have test data, we can test the classifier on them.  In our
-## example, we make one misclassification (for one of the samples in class 4.
-#test_out = testClanc(Y_test, idd, gene_names, build_out)
-      }
+  file.prefix <- paste0(file.prefix, ".sel", select.features)
+  train_out = trainClanc(Y_train, idd, gene_names)
+  build_out = buildClanc(Y_train, idd, class_names, train_out, active = select.features)
+  write.table(build_out$geneNames, paste0(file.prefix, ".genelist.txt"), row.names = F, quote=F)
+  clanc.group <- get_ClaNC_group(build_out)
+  write.csv(clanc.group, paste0(file.prefix, ".group_features.csv"), quote=F, row.names=F)
+  build_out
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# reclanc
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+reclanc is a revival of [ClaNC (Classification of microarrays to nearest
+centroids), by Alan R.
+Dabney](https://doi.org/10.1093/bioinformatics/bti681).
+
+Since the source has been lost (at least to my knowledge), the code
+comes from [here](https://github.com/naikai/sake/blob/master/R/clanc.R)
+with heavy modification.
+
+## Installation
+
+You can install the development version of reclanc like so:
+
+``` r
+# install.packages("pak")
+pak::pak("KaiAragaki/reclanc")
+```
