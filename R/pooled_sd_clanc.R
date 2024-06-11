c6cf13234385daa68f4ca3d49b3bf49da04bd630
Kai Aragaki <adamaragaki@gmail.com>
2024-06-11 11:47:42 -0400

    refresh pooled_sd_clanc

--- R/pooled_sd_clanc.R
+++ R/pooled_sd_clanc.R
@@ -1,11 +1,11 @@
-pooled_sd_clanc <- function(X, ID) {
-  m = nrow(X)
-  n = ncol(X)
-  p = ncol(ID)
-  nn = drop(t(ID) %*% rep(1, n))
-
-  mn = t(t(X %*% ID) / nn)
-  dif2 = (X - mn %*% t(ID)) ^ 2
-
-  sqrt(drop(dif2 %*% rep(1 / (n - p), n)))
+#' @param exp An expression matrix, where rows are genes and columns are
+#'   samples
+#' @param classes_one_hot A one-hot encoded matrix of classes
+#' @return A vector containing a pooled SD for each gene
+pooled_sd_clanc <- function(exp, classes_one_hot) {
+  df <- ncol(exp) - ncol(classes_one_hot)
+  samples_per_class <- colSums(classes_one_hot)
+  avg_gene_exp_per_class <- t(t(exp %*% classes_one_hot) / samples_per_class)
+  squared_error <- (exp - (avg_gene_exp_per_class %*% t(classes_one_hot)))^2
+  sqrt(rowSums(squared_error / df))
 }
