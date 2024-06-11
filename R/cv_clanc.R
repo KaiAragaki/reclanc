fb1593172cce0c25d71ecd6fd3e744707e28299d
Kai Aragaki <adamaragaki@gmail.com>
2024-06-11 09:50:05 -0400

    update names, comments

--- R/cv_clanc.R
+++ R/cv_clanc.R
@@ -5,55 +5,59 @@
 #'   proportions of each class in the training data will be used as the prior.
 #' @param active how many active features to consider? can either be a single
 #'   number or a vector containing a range of values to consider.
-cv_clanc <- function(data, id, priors = "equal", active = 1:10, folds = 5) {
-  cvIdx <- make_balanced_folds(id, folds)
+cv_clanc <- function(data, id, priors = "equal", active = 1:10, n_folds = 5) {
+  fold_indices <- make_balanced_n_folds(id, n_folds)
   nrow_data <- nrow(data)
   n <- ncol(data)
+  ncol_data <- ncol(data)
   p <- length(unique(id))
+  n_classes <- length(unique(id))
   nn <- as.numeric(table(id))
-  folds <- length(cvIdx)
+  n_folds <- length(fold_indices)
 
   class_priors <- make_class_priors(priors = priors, data = data, id = id)
   d <- length(active)
 
-  cv.error <- array(rep(0, d * folds * p), dim = c(d, folds, p))
-  cv.err.cnt.cls <- matrix(NA, nrow = d, ncol = p)
-  cv.err.prpn.cls <- matrix(NA, nrow = d, ncol = p)
-  n.features.cls <- matrix(NA, nrow = d, ncol = p)
+  # Initializations
+  cv.error <- array(rep(0, d * n_folds * n_classes), dim = c(d, n_folds, n_classes))
+  cv.err.cnt.cls <- matrix(NA, nrow = d, ncol = n_classes)
+  cv.err.prpn.cls <- matrix(NA, nrow = d, ncol = n_classes)
+  n.features.cls <- matrix(NA, nrow = d, ncol = n_classes)
   n.features.ttl <- rep(NA, d)
-
   ID <- model.matrix(~ factor(id) - 1) # one-hot encoded matrix
   dimnames(ID) <- list(NULL, names(nn))
 
   ## cross validation
   cat("CV:")
-  for(i in seq_len(folds)) {
+  for(i in seq_len(n_folds)) {
     cat(i)
 
     ## form initial statistics
-    v <- length(cvIdx[[i]]) # n samples in this fold
-    X <- data[, -cvIdx[[i]]] # samples not in this fold
-    Y <- data[, cvIdx[[i]]] # samples in this fold
-    jd <- id[-cvIdx[[i]]] # ids not in this fold
-    truth <- id[cvIdx[[i]]] # ids in this fold
-    JD <- ID[-cvIdx[[i]], ] #
+    v <- length(fold_indices[[i]]) # n samples in this fold
+    X <- data[, -fold_indices[[i]]] # samples not in this fold
+    Y <- data[, fold_indices[[i]]] # samples in this fold
+    jd <- id[-fold_indices[[i]]] # ids not in this fold
+    truth <- id[fold_indices[[i]]] # ids in this fold
+    JD <- ID[-fold_indices[[i]], ] #
 
     mm <- table(jd)
-    m.k <- sqrt(1 / mm - 1 / (n - v))
+    m.k <- sqrt(1 / mm - 1 / (ncol_data - v))
 
     ## pooled standard deviations
     p.sd <- pooledSDClanc(X, JD)
 
     ## class- and overall-centroids
     cntrd.k <- scale(X %*% JD, center = FALSE, scale = mm)
-    cntrd.o <- drop(X %*% rep(1 / (n - v), n - v))
+    cntrd.o <- drop(X %*% rep(1 / (ncol_data - v), ncol_data - v))
 
     ## form statistics and order them
     d.k <- scale((cntrd.k - cntrd.o) / p.sd, center = FALSE, scale = m.k)
     d.k.ord <- orderStatsClanc(d.k = d.k)
 
     ## select genes, update inactive centroid components
-    for(j in seq_len(d)) {
+
+    # For each item in the active vector
+    for (j in seq_len(d)) {
       aa <- active[j]
       selected <- selectClanc(d.k = d.k, d.k.ord = d.k.ord, active = aa)
       active.idx <- seq_len(nrow_data)[drop(selected %*% rep(1, p)) != 0]
