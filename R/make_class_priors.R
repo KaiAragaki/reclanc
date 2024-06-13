c34d754685ddd0b49c57b30acb4a94124f7ce138
Kai Aragaki <adamaragaki@gmail.com>
2024-06-13 14:57:50 -0400

    update make_class_priors
    Now consumes expression data and tacks on priors

--- R/make_class_priors.R
+++ R/make_class_priors.R
@@ -1,15 +1,38 @@
-make_class_priors <- function(priors, id, data) {
-  n_classes <- length(unique(id))
-  n_samples <- ncol(data)
-  samples_per_class <- as.numeric(table(id))
-  class_proportions <- n_samples / samples_per_class
+#' @param expression a data.frame containing `sample_id` and `class`, at least
+#' @param priors either a number, a numeric vector with length equal to the
+#'   number of unique classes, "equal", or "class". If "equal", the same prior
+#'   will be given to all classes. If "class", a prior based on class frequency
+#'   will be used.
+add_priors <- function(expression, priors) {
+  priors <- validate_priors(expression, priors)
 
-  if (is.numeric(priors)) {
-    stopifnot(length(priors) == n_classes, sum(priors) == 1, all(priors >= 0))
-    return(priors)
+  df <- data.frame(
+    class = levels(expression$class),
+    n = as.numeric(table(expression$class))
+  )
+
+  if (is.numeric(priors)) df$prior <- priors
+  if (priors == "equal") df$prior <- 1 / nrow(df)
+  if (priors == "class") df$prior <- df$n / nrow(df)
+
+  df <- dplyr::select(df, -.data$n)
+
+  dplyr::left_join(expression, df, by = "class")
+}
+
+validate_priors <- function(expression, priors) {
+
+  if (is.character(priors)) {
+    stopifnot(length(priors) == 1)
+    stopifnot(priors %in% c("class", "equal"))
   }
 
-  stopifnot(priors %in% c("equal", "class"))
-  if (priors == "equal") return(rep(1 / n_classes, n_classes))
-  if (priors == "class") return(class_proportions)
+  if (is.numeric(priors)) {
+    stopifnot(
+      length(priors) == length(levels(expression$class)),
+      sum(priors) == 1,
+      all(priors >= 0)
+    )
+  }
+
 }
