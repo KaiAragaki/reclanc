bbd35338cce96b4e56c581f5bd890f11f2436296
Kai Aragaki <adamaragaki@gmail.com>
2024-06-13 21:50:29 -0400

    rework order_stats

--- R/order_stats_clanc.R
+++ R/order_stats_clanc.R
@@ -1,8 +1,15 @@
-#' @param dk a matrix of statistics, where each column is a class and each row
-#'   is a gene
-order_stats_clanc <- function(dk) {
-  dk_ranks <- apply(dk, 2, \(x) order(abs(x), decreasing = TRUE))
-  dk_ordered <- apply(dk, 2, \(x) sort(abs(x), decreasing = TRUE))
-  dk_no_ties <- apply(dk_ordered, 2, \(x) !duplicated(x))
-  list(d.k.rnks = dk_ranks, d.k.o = dk_ordered, no.ties = dk_no_ties)
+add_stats <- function(expression) {
+  n_samples <- length(unique(expression$sample_id))
+  expression |>
+    dplyr::mutate(mk = sqrt(1 / dplyr::n() - 1 / n_samples), .by = "class") |>
+    dplyr::mutate(
+      stat = ((.data$class_centroid - .data$overall_centroid) / .data$pooled_sd) * .data$mk,
+      abs_stat = abs(.data$stat)
+    ) |>
+    dplyr::mutate(
+      class_rank = rank(-.data$abs_stat, ties.method = "min"),
+      no_tie = !duplicated(.data$class_rank),
+      .by = "class"
+    ) |>
+    dplyr::select(-"mk")
 }
