08ad615d2232cafa3b94582d62f1d4d3f39ecc0b
Kai Aragaki <adamaragaki@gmail.com>
2024-06-14 10:06:53 -0400

    fix gene colname

--- R/add_pooled_sd.R
+++ R/add_pooled_sd.R
@@ -6,11 +6,11 @@ add_pooled_sd <- function(expression) {
     dplyr::mutate(
       class_mean_exp = mean(.data$expression, na.rm = TRUE),
       sqd_error = (.data$expression - .data$class_mean_exp)^2,
-      .by = c("class", "gene")
+      .by = c("class", "gene_id")
     ) |>
     dplyr::mutate(
       pooled_sd = sqrt(sum(.data$sqd_error, na.rm = TRUE) / df),
-      .by = "gene"
+      .by = "gene_id"
     ) |>
     dplyr::select(-c("class_mean_exp", "squared_error"))
 }
