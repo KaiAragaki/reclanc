265290786bbed36b59ce86a868ee261ced55b8ec
Kai Aragaki <kaiaragaki@proton.me>
2024-07-19 22:58:34 -0400

    add tests

---
+++ tests/testthat/test-validate-priors.R
@@ -0,0 +1,39 @@
+test_that("char priors processed correctly", {
+  expect_equal(
+    process_priors_char("equal", factor(c("a", "b", "c", "a"))),
+    c(1 / 3, 1 / 3, 1 / 3)
+  )
+  expect_equal(
+    process_priors_char("class", factor(c("a", "b", "c", "a"))),
+    c(1 / 2, 1 / 4, 1 / 4)
+  )
+  expect_equal(
+    process_priors_char(
+      "class",
+      factor(c("a", "b", "c", "a"), levels = c("c", "b", "a"))
+    ),
+    c(1 / 4, 1 / 4, 1 / 2)
+  )
+  expect_equal(
+    process_priors_char("equal", factor("a")),
+    1
+  )
+  expect_equal(
+    process_priors_char("equal", factor(c("a", "a"))),
+    1
+  )
+  expect_error(
+    proccess_priors_char(c("equal", "equal"), factor("a"))
+  )
+})
+
+test_that("num priors processed correctly", {
+  expect_equal(process_priors_num(1, factor("a")), 1)
+  expect_warning(process_priors_num(0.9, factor("a")))
+  expect_error(process_priors_num(-1, factor("a")))
+  expect_error(process_priors_num(2, factor("a")))
+  expect_error(process_priors_num(c(-1, 2), factor(c("a", "b"))))
+  expect_warning(process_priors_num(c(0.5, 0.4), factor(c("a", "b"))))
+  expect_error(process_priors_num(c(0.5, 0.6), factor(c("a", "b"))))
+  expect_no_error(process_priors_num(1.000001, factor("a")))
+})
